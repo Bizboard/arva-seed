ce2f9717abc03df4c97f95a05667d2323ced4863
Tom Clement <mr@tomclement.nl>
2015-12-09 12:22:00 +0100

    Updated JSPM version, removed now obsolete css and image loaders.

--- src/css.js
+++
@@ -1,2 +0,0 @@
-var _ = require('lodash');
-exports = _.extend(exports, require('css/css.js'));
--- src/image.js
+++
@@ -1,2 +0,0 @@
-var _ = require('lodash');
-exports = _.extend(exports, require('arva-utils/ImageLoader.js'));
