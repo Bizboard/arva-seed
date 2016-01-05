2fa6cae59199e9ed8a9d9716434912f4dc471201
Hans van den Akker <hans@bizboard.nl>
2016-01-05 17:37:32 +0100

    Update cordova.js
    Polyfill CustomEvent for broader browser support.

--- www/cordova.js
+++ www/cordova.js
@@ -1,13 +1,28 @@
 /* Just a dummy file to allow for in-browser development, will be replaced in app builds by running 'cordova build' */
-if(!window.cordovaDummyInitialized){
+if (!window.cordovaDummyInitialized) {
 
-    var existingOnload = window.onload || function(){};
+    try {
+        var throwsErrorOnIE = new CustomEvent('deviceready');
+    } catch (error) {
+        /* Polyfill CustomEvent for IE */
+        function CustomEvent(event, params) {
+            params = params || {bubbles: false, cancelable: false, detail: undefined};
+            var evt = document.createEvent('CustomEvent');
+            evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
+            return evt;
+        }
 
-    window.onload = function(){
+        CustomEvent.prototype = window.Event.prototype;
+        window.CustomEvent = CustomEvent;
+    }
+
+    var existingOnload = window.onload || function () {};
+
+    window.onload = function () {
         existingOnload();
 
         /* Emulate Cordova behaviour for in-browser testing */
-        document.dispatchEvent(new Event('deviceready'));
+        document.dispatchEvent(new CustomEvent('deviceready'));
     };
 
     window.cordovaDummyInitialized = true;
