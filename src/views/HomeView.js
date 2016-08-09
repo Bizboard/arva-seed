bf11e3ed0b1764f0824cb1c4fa7b4ab8d3138376
Karl Lundfall <karl@bizboard.nl>
2016-08-09 09:58:40 +0200

    Added new line to make imports look more proper

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,4 +1,5 @@
 import Surface              from 'famous/core/Surface.js';
+
 import {View}               from 'arva-js/core/View.js';
 import {layout, options}    from 'arva-js/layout/decorators.js';
 
