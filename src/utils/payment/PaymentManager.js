b59012ef7406f6e8e146badb596d61ac33249c84
Karl Lundfall <karl@bizboard.nl>
2016-11-30 09:12:05 +0100

    Changed name of payment propery

--- src/utils/payment/PaymentManager.js
+++ src/utils/payment/PaymentManager.js
@@ -2,9 +2,8 @@
  * Created by lundfall on 16/11/2016.
  */
 import {Injection}                          from 'arva-js/utils/Injection.js';
-import {DataSource}                         from 'arva-js/data/DataSource.js';
 import {PaymentRequest, Payments, Payment}  from 'mrbox-shared/models/Payments.js';
-import {BuckarooEnvironment}                from './BuckarooEnvironment.js';
+import {ExternalEnvironment}                from './ExternalEnvironment.js';
 import {PromiseBasedManager}                from '../PromiseBasedManager.js';
 
 const agentTimeout = 20000;
@@ -13,10 +12,9 @@ export class PaymentManager extends PromiseBasedManager {
 
     constructor() {
         super();
-        this._buckarooEnvironment = Injection.get(BuckarooEnvironment);
+        this._externalEnvironment = Injection.get(ExternalEnvironment);
         this._initialized = this._dataSource.getAuth().then((auth) => {
             this._userID = auth.uid;
-            this._payments = new Payments();
         });
     }
 
@@ -24,24 +22,25 @@ export class PaymentManager extends PromiseBasedManager {
         if (this._payment) {
             throw new Error("Payment is already in progress");
         }
-        this._buckarooEnvironment.closeIfOpen();
+        this._externalEnvironment.closeIfOpen();
         await this._initialized;
         this._payment = new Payment(paymentID);
         console.log("Finding pending payment...");
-        //TODO: Decide how to make this work...
-        await this._promiseWithTimeout(this._payment.once('changed'), agentTimeout);
+        await this._promiseWithTimeout(this._payment.once('value'), agentTimeout);
         if (this._payment.status === 'pending') {
             console.log("Waiting for the agent to recognize the payment...");
-            let payment = await this._promiseWithTimeout(this._payment.once('value'), agentTimeout, "Waiting for payment agent");
+            //TODO: Decide how to make this work...
+            let payment = await this._promiseWithTimeout(this._payment.once('changed'), agentTimeout, "Waiting for payment agent");
         }
 
         switch (this._payment.status) {
             case 'acknowledged':
-                if (!this._payment.requestObject) {
+                if (!this._payment.htmlPostRequest) {
                     throw new Error("Got acknowledged response but no valid request object");
                 } else {
                     console.log("Opening buckaroo environment...");
                     /* TODO: Change second argument to false for live environemnt*/
+                    await this._externalEnvironment.openBuckarooPayment(this._payment.htmlPostRequest, true);
                 }
                 break;
             case 'failed':
@@ -50,12 +49,13 @@ export class PaymentManager extends PromiseBasedManager {
             default:
                 throw new Error(`Server responded with unexpected status: ${this._payment.status}`)
         }
-        let buckarooEnvironmentPromise = this._buckarooEnvironment.openBuckarooEnvironment(this._payment.requestObject, true);
-
-        if (this._payment.status === 'acknowledged') {
-            await this._promiseWithTimeout(Promise.all([this._payment.synced(), buckarooEnvironmentPromise]), agentTimeout, "Get payment confirmation from agent");
-            this._buckarooEnvironment.closeIfOpen();
+        /* If the payment is still processing */
+        while(this._payment.status === 'acknowledged' || this._payment.status === 'processing'){
+            await this._promiseWithTimeout(this._payment.synced(), agentTimeout, "Get payment confirmation from agent");
         }
+
+        this._externalEnvironment.closeIfOpen();
+
         switch (this._payment.status) {
             case 'rejected':
                 throw new Error(`Payment rejected, reason: ${this._payment.reason}`);
